f7e225cc5ed716742aea5b23b3194046f24151e3
JocobZling <924798429@qq.com>
2019-05-31 23:52:15 +0800

    Add function demo1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,28 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+  let numArray = num.split("");
+  let result = "";
+  let template = {
+    0: "._.\n|.|\n|_|",
+    1: `...\n..|\n..|`,
+    2: `._.\n._|\n|_.`,
+    3: `._.\n._|\n._|`,
+    4: `...\n|_|\n..|`,
+    5: `._.\n|_.\n._|`,
+    6: `._.\n|_.\n|_|`,
+    7: `._.\n..|\n..|`,
+    8: `._.\n|_|\n|_|`,
+    9: `._.\n|_|\n..|`
+  };
+  for (let i = 0; i < 3; i++) {
+    numArray.forEach((item, flag) => {
+      if (flag !== numArray.length) {
+        result += template[item].split("\n")[i] + " ";
+      } else {
+        result += template[item].split("\n")[i];
+      }
+    });
+    result += "\n";
+  }
+  console.log(result);
+  return result;
 };
